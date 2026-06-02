c8d47f3f0a2e3e77279f7e03787492c8973e132b
olsaa0 <166027576+olsaa0@users.noreply.github.com>
2026-06-02 16:08:09 +0200

    Add files via upload

---
+++ module7/SecondScreen.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { View , Text, Button, StyleSheet} from "react-native";
+
+const FirstScreen = (props) => {
+    console.log(props);
+
+    return (
+        <View style={StyleSheet.container}>
+            <Text style={StyleSheet.text}>First Screen</Text>
+            <Button title="Go to the 2 Screen" onPress={() => {props.navigation.goBack("First")}}/>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    conatiner: {
+        flex: 1,
+        justifyContent: "center",
+        alignItemms: "center",
+        backgroundcolor: "lightblue"
+    },
+    text: {
+        fontSize: 30,
+        fontWeight: "bold",
+        color: "#333",
+        padding: 10,
+        textAlign: "center"
+    }
+});
+
+export default FirstScreen;

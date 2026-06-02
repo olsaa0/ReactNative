301c7dad492b3ab4bb2d60f08814fc14b923dedd
olsaa0 <166027576+olsaa0@users.noreply.github.com>
2026-06-02 16:10:47 +0200

    app.js

--- module7/App.js
+++ module7/App.js
@@ -14,6 +14,7 @@ export default function App() {
         |<Stack.Screen name="First" component={FirstScreen} />
         <Stack.Screen name="Second" component={SecondScreen} />
       </Stack.Navigator>
+    <StatusBar style="auto" />
     </Navigationcontainer>
     
   );

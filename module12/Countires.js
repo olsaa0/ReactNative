58f3850a164dda88203edee911ca28e6775d0ef6
olsaa0 <166027576+olsaa0@users.noreply.github.com>
2026-06-16 16:14:48 +0200

    Add Countries component to display country data

---
+++ module12/Countires.js
@@ -0,0 +1,22 @@
+import React from "react";
+import {View, Text, FlatList, Stylesheet} from 'react-native';
+import data from '../assets/data.json';
+
+const Countries = () => {
+    return (
+        <View>
+            <Text>FlatList</Text>
+            <FlatList
+            data={data}
+            renderItem={({item}) => {
+                return(
+                    <View>
+                        <Text>{item.country}</Text>
+                        <Text>{item.capital}</Text>
+                    </View>
+                );
+            }}/>
+        </View>
+    );
+}
+export default Countries;
